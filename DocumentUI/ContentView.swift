c56845341641c23efd362c35bb75251f1a1558b2
Guru Mahan <iosdeve2022@gmail.com>
2023-04-03 18:14:44 +0530

    whitespace removed

--- DocumentUI/ContentView.swift
+++ DocumentUI/ContentView.swift
@@ -8,24 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
-   @ObservedObject var viewModel = DocumentViewModel()
-  @State var isSelected = false
+    
+    @ObservedObject var viewModel = DocumentViewModel()
+    @State var isSelected = false
     var body: some View {
         NavigationView {
             ZStack {
                 LinearGradient(colors: [Color.gray.opacity(0.2)], startPoint: .leading, endPoint: .trailing)
                 VStack {
-                    
                     headerView
                         .frame(maxWidth: .infinity)
                         .background(Color.white)
-                    
                     ScrollView{
                         ForEach(viewModel.documentModel, id:\.id) { doc in
                             if doc.url.isEmpty {
-                                
                             } else {
-                                
                             }
                         }
                         frontView
@@ -34,48 +31,28 @@ struct ContentView: View {
                             .padding([.top,.bottom],3)
                             .padding(.horizontal,7)
                     }
-                  //Spacer()
-                    
-                     bottomView
+                    bottomView
                         .frame(width: UIScreen.main.bounds.width)
                         .background(Color.white)
-                    
                 }
-                
-                
             }
         }
         .fileImporter(isPresented: $isSelected, allowedContentTypes: [.image,.audio,.data]) { result in
-            
             do {
-
                 let furl = try result.get()
                 print("")
                 viewModel.documentModel[0].url = furl.lastPathComponent
                 viewModel.documentModel[0].pathExtension = furl.pathExtension
                 print("file Ext \(furl.pathExtension)")
-             //   print("Url \(getUrl)")
-//                if furl.startAccessingSecurityScopedResource(){
-//                    let data = try Data(contentsOf: furl)
-//
-//                    furl.stopAccessingSecurityScopedResource()
-//                }
-
-
             } catch {
                 print("error: \(error)") // todo
             }
-
         }
-        
     }
     
     @ViewBuilder var headerView: some View {
-        
         HStack {
-            
             Button {
-                
             } label: {
                 HStack{
                     Image(systemName: "chevron.backward")
@@ -83,38 +60,20 @@ struct ContentView: View {
                     Text("Back")
                         .foregroundColor(.blue)
                         .font(.headline)
-                    
                 }
                 
             }.padding(.leading,10)
             Spacer()
-            
-            
                 .overlay( Text("Document")
                     .padding(.horizontal,-99) , alignment: .center )
                 .font(Font.system(size: 30))
-            //                .alignmentGuide(VerticalAlignment.center) {   // << here !!
-            //                                   $0[VerticalAlignment.top]
-            //                               }
-            //  .frame(alignment: .center)
-            
-            
-            //  Spacer()
-            
         }
         .padding(10)
-        
     }
-   
     
-
-
     @ViewBuilder var bottomView: some View {
-        
         HStack(spacing: 30){
-            
             Button {
-                
             } label: {
                 Text("Back")
                     .frame(maxWidth: .infinity )
@@ -123,13 +82,8 @@ struct ContentView: View {
                     .overlay( /// apply a rounded border
                         RoundedRectangle(cornerRadius: 20)
                             .stroke(.blue, lineWidth: 2))
-                
-                
             }
-            
-            //  .padding(.horizontal,30)
             Button {
-                
             } label: {
                 Text("Submit")
                     .frame(height: 40)
@@ -139,13 +93,11 @@ struct ContentView: View {
                         RoundedRectangle(cornerRadius: 20)
                             .stroke(.gray, lineWidth: 2))
             }
-            //  .padding(.horizontal,30)
         }
         .padding()
     }
     
     @ViewBuilder var frontView: some View{
-        
         //MARK: ->ID Proof Stack
         VStack(alignment: .leading,spacing: 10){
             Text("Accepted Formats(JPG,PNG) - Upload File Max Size (6mb)")
@@ -153,10 +105,7 @@ struct ContentView: View {
             Text("ID Proof")
                 .font(.headline)
             HStack{
-                
-                
                 Text("Resident / National id")
-                // .font(Font.subheadline)
                     .font(Font.system(size: 12))
                     .fontWeight(.light)
                 Spacer()
@@ -164,26 +113,17 @@ struct ContentView: View {
                     .font(Font.system(size: 12))
                     .fontWeight(.light)
             }
-            
-            
             HStack(spacing: 15){
                 Button {
-                    
                 } label: {
                     Image("pdf")
                         .resizable()
-                    // .aspectRatio(contentMode: .fill)
                         .frame(width: 30,height: 30)
-                    // .background(Color.gray)
                         .clipped()
                 } .frame(width: 50)
                     .frame(maxHeight: .infinity)
                     .background(Color.gray.opacity(0.3))
-                
-                //  .padding(.vertical, 12)
-                
                 VStack(alignment:.leading,spacing: 3){
-                    
                     Text("Resident / National id")
                         .padding(.vertical,8)
                     Text("File name123ABC.pdf")
@@ -193,14 +133,12 @@ struct ContentView: View {
                 }
                 Spacer()
                 Button {
-                    
                 } label: {
                     Image(systemName: "xmark")
                         .foregroundColor(.black)
                         .font(Font.system(size: 20))
                 }
                 .padding()
-                
             }
             .frame(maxWidth: .infinity)
             .background(Color.gray.opacity(0.09))
@@ -208,9 +146,7 @@ struct ContentView: View {
             Divider().frame(height: 0.5).overlay(.black)
             
             //MARK: ->Addrress Details Stack
-            
             VStack(alignment: .leading,spacing: 5){
-                
                 Text("Addrress Details")
                     .font(.headline)
                     .font(Font.system(size: 12))
@@ -223,71 +159,56 @@ struct ContentView: View {
                         .font(Font.system(size: 12))
                         .fontWeight(.light)
                 }
-               
                 if viewModel.documentModel[0].url != "" {
-                        HStack(spacing: 15){
-                            Button {
-                           
-                            } label: {
-                                Text(viewModel.documentModel[0].pathExtension).font(Font.system(size: 20))
-                                    .fontWeight(.bold)
-                                    .foregroundColor(.red)
-                                
-                            } .frame(width: 50)
-                                .frame(maxHeight: .infinity)
-                                .background(Color.gray.opacity(0.3))
+                    HStack(spacing: 15){
+                        Button {
+                        } label: {
+                            Text(viewModel.documentModel[0].pathExtension).font(Font.system(size: 20))
+                                .fontWeight(.bold)
+                                .foregroundColor(.red)
                             
-                            //  .padding(.vertical, 12)
-                            
-                            VStack(alignment:.leading,spacing: 3){
-                                
-                                Text(viewModel.documentModel[0].url)
-                                    .padding(.vertical,8)
-                                //                                Text("File name123ABC.pdf")
-                                //                                    .fontWeight(.light)
-                                //                                    .font(Font.system(size: 14))
-                                //                                    .padding(.vertical,1)
-                            }
-                            Spacer()
-                            Button {
-                                viewModel.documentModel[0].url  = ""
-                            } label: {
-                                Image(systemName: "xmark")
-                                    .foregroundColor(.black)
-                                    .font(Font.system(size: 20))
-                            }
-                            .padding()
-                            
-                        }.frame(maxWidth: .infinity)
-                            .background(Color.gray.opacity(0.09))
-                            .cornerRadius(5)
-                        
-                    }else{
-                        HStack{
+                        } .frame(width: 50)
+                            .frame(maxHeight: .infinity)
+                            .background(Color.gray.opacity(0.3))
+                        VStack(alignment:.leading,spacing: 3){
+                            Text(viewModel.documentModel[0].url)
+                                .padding(.vertical,8)
+                        }
+                        Spacer()
+                        Button {
+                            viewModel.documentModel[0].url  = ""
+                        } label: {
+                            Image(systemName: "xmark")
+                                .foregroundColor(.black)
+                                .font(Font.system(size: 20))
+                        }
+                        .padding()
+                    }.frame(maxWidth: .infinity)
+                        .background(Color.gray.opacity(0.09))
+                        .cornerRadius(5)
+                }else{
+                    HStack{
                         Text("Upload Postal Address")
                         Spacer()
                         Button {
-                          isSelected = true
+                            isSelected = true
                         } label: {
                             Image(systemName: "arrow.up.to.line")
                                 .font(Font.system(size: 25))
                         }
-                        
                     } .padding()
-                            .fontWeight(.light)
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 15)
-                                    .stroke(style: StrokeStyle(lineWidth: 2, dash: [5.0]))
-                                    .foregroundColor(.gray.opacity(0.3)) )
+                        .fontWeight(.light)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(style: StrokeStyle(lineWidth: 2, dash: [5.0]))
+                                .foregroundColor(.gray.opacity(0.3)) )
                 }
-               
             }
             .padding(2)
             Divider().frame(height: 0.5).overlay(.black)
             
             //MARK: ->Employment details Stack
             VStack(alignment: .leading,spacing:5){
-                
                 Text("Employment details")
                     .font(.headline)
                     .font(Font.system(size: 12))
@@ -301,18 +222,15 @@ struct ContentView: View {
                             .font(Font.system(size: 12))
                             .fontWeight(.light)
                     }
-                    
                     HStack{
                         Text("Upload Employee ID")
                         Spacer()
                         Button {
-                      isSelected = true
+                            isSelected = true
                         } label: {
                             Image(systemName: "arrow.up.to.line")
                                 .font(Font.system(size: 25))
                         }
-                        
-                        
                     }
                     .padding()
                     .fontWeight(.light)
@@ -320,10 +238,7 @@ struct ContentView: View {
                         RoundedRectangle(cornerRadius: 15)
                             .stroke(style: StrokeStyle(lineWidth: 2, dash: [5.0]))
                             .foregroundColor(.gray.opacity(0.3))
-                        
                     )
-                    
-                    
                     HStack{
                         Text("Appointment Order")
                             .font(Font.system(size: 12))
@@ -337,13 +252,11 @@ struct ContentView: View {
                         Text("Upload Appointment Order")
                         Spacer()
                         Button {
-                           isSelected = true
+                            isSelected = true
                         } label: {
                             Image(systemName: "arrow.up.to.line")
                                 .font(Font.system(size: 25))
                         }
-                        
-                        
                     }
                     .padding()
                     .fontWeight(.light)
@@ -351,17 +264,13 @@ struct ContentView: View {
                         RoundedRectangle(cornerRadius: 15)
                             .stroke(style: StrokeStyle(lineWidth: 2, dash: [5.0]))
                             .foregroundColor(.gray.opacity(0.3))
-                        
                     )
                 }.padding(.vertical,8)
-                
-                
             }
             Divider().frame(height: 0.5).overlay(.black)
             
             //MARK: ->Medical Details Stack
             VStack(alignment: .leading,spacing:5){
-                
                 Text("Medical Details")
                     .font(.headline)
                     .font(Font.system(size: 12))
@@ -375,18 +284,15 @@ struct ContentView: View {
                             .font(Font.system(size: 12))
                             .fontWeight(.light)
                     }
-                    
                     HStack{
                         Text("Upload Health Passport")
                         Spacer()
                         Button {
-                         isSelected = true
+                            isSelected = true
                         } label: {
                             Image(systemName: "arrow.up.to.line")
                                 .font(Font.system(size: 25))
                         }
-                        
-                        
                     }
                     .padding()
                     .fontWeight(.light)
@@ -394,10 +300,7 @@ struct ContentView: View {
                         RoundedRectangle(cornerRadius: 15)
                             .stroke(style: StrokeStyle(lineWidth: 2, dash: [5.0]))
                             .foregroundColor(.gray.opacity(0.3))
-                        
                     )
-                    
-                    
                     HStack{
                         Text("Vaccination - QR Code(Optional)")
                             .font(Font.system(size: 12))
@@ -411,13 +314,11 @@ struct ContentView: View {
                         Text("Upload Vaccination - QR Code")
                         Spacer()
                         Button {
-                          isSelected = true
+                            isSelected = true
                         } label: {
                             Image(systemName: "arrow.up.to.line")
                                 .font(Font.system(size: 25))
                         }
-                        
-                        
                     }
                     .padding()
                     .fontWeight(.light)
@@ -425,16 +326,10 @@ struct ContentView: View {
                         RoundedRectangle(cornerRadius: 15)
                             .stroke(style: StrokeStyle(lineWidth: 2, dash: [5.0]))
                             .foregroundColor(.gray.opacity(0.3))
-                        
                     )
                 }.padding(.vertical,8)
-                
-                
             }
-            
         }
-        //  Spacer()
-        
         .padding(10)
     }
 }
